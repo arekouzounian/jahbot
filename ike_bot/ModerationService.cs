40762f91461e52cf9c5efebf5fbbefc776f4f75c
Arek Ouzounian <agouzo777@gmail.com>
2019-07-10 13:48:08 -0700

    hopefully fixing rename stuff for deploy

--- ike_bot/ModerationService.cs
+++ ike_bot/ModerationService.cs
@@ -40,6 +40,7 @@ namespace ike_bot
             foreach(var user in users)
             {
                 await RenameUser((user as SocketGuildUser), newName);
+                await Task.Delay(200);
             }
         }
     }
